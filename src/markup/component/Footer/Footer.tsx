e1a60bc2c15cdb440553aee94a8ef3a103e76e3f
ayfokir <ayfotsega19.27@gmail.com>
2024-06-24 14:16:56 +0300

    remove scroll horizontally

--- src/markup/component/Footer/Footer.tsx
+++ src/markup/component/Footer/Footer.tsx
@@ -2,14 +2,16 @@
 import { css } from '@emotion/react';
 import React from 'react';
 
+
 const footerStyles = css`
   background-color: #333;
   color: #fff;
   padding: 10px 20px;
   text-align: center;
   bottom: 0;
-  width: 100%;
-  margin-top: 20px; /* Add margin top */
+  width: 100%; /* Ensure footer spans the full width */
+  box-sizing: border-box; /* Include padding in the element's total width */
+  margin-top: 20px;
 `;
 
 const Footer: React.FC = () => (
